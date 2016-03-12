39a4099403374bf7db58155dab7aa3b2a217fa79
xuke918 <xuke3070@gmail.com>
2016-03-12 00:01:22 -0500

    Added semicolon and put var on the top.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var $h1 = $('h1');
+var $heading = $('heading');
+
 $h1.html('Below ground veggies');
 $('body').prepend('<h1>Below ground veggies</h1>');
 
-var $heading = $('heading')
 $heading.html('Adove ground veggies');
 $('ul').append('<h1>Adove ground veggies</h1>');
