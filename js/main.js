3f8f4220b034a756eee02d03f21149023aa25aa3
xuke918 <xuke3070@gmail.com>
2016-03-12 00:00:07 -0500

    Create new heading.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var $h1 = $('h1');
+$h1.html('Below ground veggies');
+$('body').prepend('<h1>Below ground veggies</h1>');
+
+var $heading = $('heading')
+$heading.html('Adove ground veggies');
+$('ul').append('<h1>Adove ground veggies</h1>');
