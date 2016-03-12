8792074d22c86383897a11bb4355e5752fcd407a
xuke918 <xuke3070@gmail.com>
2016-03-12 00:12:08 -0500

    Added above sentence.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
 var $h1 = $('h1');
 var $heading = $('heading');
+var $li = $('.above');
 
 $h1.html('Below ground veggies');
 $('body').prepend('<h1>Below ground veggies</h1>');
 
 $heading.html('Adove ground veggies');
-$('ul').append('<h1>Adove ground veggies</h1>');
+$('body').append('<h1>Adove ground veggies</h1>');
