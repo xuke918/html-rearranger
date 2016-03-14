d9927dd6de47829627047833ca4d15af811fab7e
xuke918 <xuke3070@gmail.com>
2016-03-14 17:38:57 -0400

    Fixed the code.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,20 @@
-var $h1Below = $('h1');
-var $h1Above = $('h1');
-var $liAbove = $('.above');
-var $liBelow = $('.below');
+var $h1Below = $('<h1>');
+var $h1Above = $('<h1>');
+var $liAbove = $('<ul>');
+var $liBelow = $('<ul>');
 
 $h1Below.html('Below ground veggies');
-$('body').prepend('<h1>Below ground veggies</h1>');
+$('body').append($h1Below);
+$('body').append($liAbove);
 
-$h1Above.html('Adove ground veggies');
-$('body').append('<h1>Above ground veggies</h1>');
+$h1Above.html('Above ground veggies');
+$('body').append($h1Above);
+$('body').append($liBelow);
 
-$('ul').append($liAbove);
+$('ul li').each(function () {
+  if ($(this).hasClass('below')) {
+    $liAbove.append($(this));
+  } else {
+    $liBelow.append($(this));
+  }
+});
